e5fa45190941d4e86d6f1531eee7c003cc7999a6
Davin <85967571+dav1n9@users.noreply.github.com>
2024-01-07 18:18:03 +0900

    feat: 블로그 글 목록 조회를 위한 API 구현

--- src/main/java/me/davin/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/davin/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.davin.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.davin.springbootdeveloper.domain.Article;
 import me.davin.springbootdeveloper.dto.AddArticleRequest;
+import me.davin.springbootdeveloper.dto.ArticleResponse;
 import me.davin.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body 에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -22,4 +26,15 @@ public class BlogApiController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)  // 기존의 각 Article 을 ArticleResponse 로 매핑
+                .toList();
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
 }
