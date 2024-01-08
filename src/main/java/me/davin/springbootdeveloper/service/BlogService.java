a22abf0b631f3a191a622a618e1f27f30ad5dbbd
Davin <85967571+dav1n9@users.noreply.github.com>
2024-01-08 15:29:39 +0900

    feat: 블로그 글 수정 API 추가

--- src/main/java/me/davin/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/davin/springbootdeveloper/service/BlogService.java
@@ -3,8 +3,10 @@ package me.davin.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.davin.springbootdeveloper.domain.Article;
 import me.davin.springbootdeveloper.dto.AddArticleRequest;
+import me.davin.springbootdeveloper.dto.UpdateArticleRequest;
 import me.davin.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -29,5 +31,15 @@ public class BlogService {
 
     public void delete(long id) {
         blogRepository.deleteById(id);
+    }
+
+    @Transactional  // 매칭한 메서드를 하나의 트랜젝션으로 묶는 역할
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
