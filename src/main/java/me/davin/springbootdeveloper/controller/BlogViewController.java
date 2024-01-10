4b8e0fba13f1f8376f7bfe09fe0bee882de1f3ff
Davin <85967571+dav1n9@users.noreply.github.com>
2024-01-10 14:20:48 +0900

    feat: 블로그 글 뷰 구현
    블로그 글이 보이도록 블로그 글 뷰를 구현함.
    엔티티에 생성시간, 수정 시간을 추가하고, 컨트롤러 메서드를 만든 다음 HTML 뷰를 만들고 확인함.

--- src/main/java/me/davin/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/davin/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.davin.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.davin.springbootdeveloper.domain.Article;
 import me.davin.springbootdeveloper.dto.ArticleListViewResponse;
+import me.davin.springbootdeveloper.dto.ArticleViewResponse;
 import me.davin.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
