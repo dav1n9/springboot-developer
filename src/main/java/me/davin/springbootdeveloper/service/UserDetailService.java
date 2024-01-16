d31cdbaa10ed524e618ecfd8ae0d6460f92b6fdb
Davin <85967571+dav1n9@users.noreply.github.com>
2024-01-16 17:04:16 +0900

    feat: 회원 도메인 추가
    1. 회원 엔티티와 리포지터리를 추가
    2. UserDetailService 서비스 메서드 코드 작성: 스프링 시큐리티에서 로그인을 진행할 때 사용자 정보를 가져오는 코드를 작성함.

---
+++ src/main/java/me/davin/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package me.davin.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.davin.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {  // 스프링 시큐리티에서 사용자의 정보를 가져오는 인터페이스
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
