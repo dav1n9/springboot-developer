08ee6d675dfb9cb37dfaba308b2579280daeffa5
Davin <85967571+dav1n9@users.noreply.github.com>
2024-01-28 21:30:31 +0900

    feat: 리프레시 토큰 도메인 구현

---
+++ src/main/java/me/davin/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package me.davin.springbootdeveloper.repository;
+
+import me.davin.springbootdeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
